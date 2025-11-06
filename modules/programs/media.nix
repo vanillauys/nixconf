9c4f7fd78d8d72cffb0c532cc17e68dc2ef0062f
wihanuys <wihan@glydepay.io>
2025-11-06 14:55:40 +0200

    updates

--- modules/programs/media.nix
+++ modules/programs/media.nix
@@ -31,7 +31,7 @@ in {
       extraPackages = with pkgs; [
         intel-media-driver
         intel-vaapi-driver
-        vaapiVdpau
+        libva-vdpau-driver
         libvdpau-va-gl
       ];
     };
